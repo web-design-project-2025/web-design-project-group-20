2c8d8086c1432e3648a2516c3ce8077f9e9f1e78
Ebba555 <ceeb24wi@student.ju.se>
2025-04-17 11:06:30 +0200

    loading-article-branch, articles-list, and more

---
+++ javascript/load-articles.js
@@ -0,0 +1,38 @@
+let articles = [];
+const contentElement = document.getElementById("articles_content");
+
+async function loadData() {
+  const articleResponse = await fetch("data/articles.json");
+  const articleJSON = await articleResponse.json();
+  articles = articleJSON.articles;
+
+  renderContent();
+}
+
+function createArticleElement(article) {
+  const articleElement = document.createElement("article");
+  articleElement.classList.add("article-box");
+
+  const iconElement = document.createElement("img");
+  iconElement.scr = article.icon;
+  iconElement.classList.add("icons");
+  articleElement.appendChild(iconElement);
+
+  const articleTitle = document.createElement("p");
+  articleTitle.innerText = article.title;
+  articleTitle.classList.add("article-name");
+  articleElement.appendChild(articleTitle);
+
+  return articleElement;
+}
+
+function renderContent() {
+  contentElement.innerHTML = "";
+
+  for (let article of articles) {
+    const articleElement = createArticleElement(article);
+    contentElement.appendChild(articleElement);
+  }
+}
+
+loadData();
