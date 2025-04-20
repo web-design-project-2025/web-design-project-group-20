3a99ce2800c926848a015f9a6155013dc286b54e
WilmaNyren <nywi24wv@student.ju.se>
2025-04-20 14:01:53 +0200

    dark/light icons alignment in settings

--- javascript/account.js
+++ javascript/account.js
@@ -17,15 +17,17 @@ document.addEventListener("DOMContentLoaded", () => {
   settingsEliment.addEventListener("click", function (event) {
     contentElement.innerHTML = `
             <section>
-                <h2>Settings</h2>
-                 <div class="settings-list"> 
-                    <p>Light mode:</p> 
-                    <img class="account-icons" id="light" src="icons/star-full.svg" alt="light mode button">
-                <div> 
-                <div class="settings-list"> 
-                    <p>Dark mode:</p> 
-                     <img class="account-icons" id="dark" src="icons/star-hallow.svg" alt="dark mode button">
-                <div> 
+              <h2>Settings</h2>
+              <div id="bowl"> 
+                <div id="settings-list1"> 
+                  <p>Light mode:</p> 
+                  <img class="account-icons" id="light" src="icons/sun-green.svg" alt="light mode button">
+                </div> 
+                <div id="settings-list2"> 
+                  <p>Dark mode:</p> 
+                  <img class="account-icons" id="dark" src="icons/moon-green.svg" alt="dark mode button">
+                </div> 
+              </div> 
             </section>
   `;
 
