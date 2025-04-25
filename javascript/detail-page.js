5679f410eb5daf5fdae871e11cb71152577f16ee
juliatrue <trju24cc@student.ju.se>
2025-04-25 13:30:00 +0200

    added detail page for articles, only shows icon for now

--- javascript/detail-page.js
+++ javascript/detail-page.js
@@ -1,86 +1,102 @@
 let films = [];
+let articles = [];
 
 document.addEventListener("DOMContentLoaded", async function () {
   const filmImg = document.getElementById("details");
   const filmTitle = document.getElementById("detail-title");
   const filmDescription = document.getElementById("detail-description");
   const filmStars = document.getElementById("detail-star-container");
+  const articleImg = document.getElementById("detail-article");
 
   function getQueryParam(param) {
     const urlParams = new URLSearchParams(document.location.search);
     return urlParams.get(param);
   }
 
-  const whatFilm = getQueryParam("title");
-  console.log(whatFilm);
+  if (document.getElementById("details")) {
+    const whatFilm = getQueryParam("title");
+    console.log(whatFilm);
 
-  const filmResponse = await fetch("data/movies.json");
-  const filmJSON = await filmResponse.json();
-  films = filmJSON.movies;
+    const filmResponse = await fetch("data/movies.json");
+    const filmJSON = await filmResponse.json();
+    films = filmJSON.movies;
 
-  const film = films.find((m) => m.title == whatFilm);
+    const film = films.find((m) => m.title == whatFilm);
 
-  function filmFullStar() {
-    const filmFullStar = document.createElement("img");
-    filmFullStar.classList.add("film-stars");
-    filmFullStar.src = "icons/star-full.svg";
-    filmStars.appendChild(filmFullStar);
-  }
+    function filmFullStar() {
+      const filmFullStar = document.createElement("img");
+      filmFullStar.classList.add("film-stars");
+      filmFullStar.src = "icons/star-full.svg";
+      filmStars.appendChild(filmFullStar);
+    }
 
-  function filmHollowStar() {
-    const filmHollowStar = document.createElement("img");
-    filmHollowStar.classList.add("film-stars");
-    filmHollowStar.src = "icons/star-hallow.svg";
-    filmStars.appendChild(filmHollowStar);
-  }
+    function filmHollowStar() {
+      const filmHollowStar = document.createElement("img");
+      filmHollowStar.classList.add("film-stars");
+      filmHollowStar.src = "icons/star-hallow.svg";
+      filmStars.appendChild(filmHollowStar);
+    }
 
-  function numberOfReviews() {
-    const reviewElement = document.createElement("p");
-    reviewElement.innerHTML = film.nr_of_reviews;
-    reviewElement.classList.add("film-number-of-reviews");
-    filmStars.appendChild(reviewElement);
-  }
+    function numberOfReviews() {
+      const reviewElement = document.createElement("p");
+      reviewElement.innerHTML = film.nr_of_reviews;
+      reviewElement.classList.add("film-number-of-reviews");
+      filmStars.appendChild(reviewElement);
+    }
 
-  if (film) {
-    filmImg.src = film.image;
-    filmTitle.innerText = film.title;
-    filmDescription.innerText = film.description;
+    if (film) {
+      filmImg.src = film.image;
+      filmTitle.innerText = film.title;
+      filmDescription.innerText = film.description;
 
-    if (film.rating === 1) {
-      filmFullStar();
-      filmHollowStar();
-      filmHollowStar();
-      filmHollowStar();
-      filmHollowStar();
-      numberOfReviews();
-    } else if (film.rating === 2) {
-      filmFullStar();
-      filmFullStar();
-      filmHollowStar();
-      filmHollowStar();
-      filmHollowStar();
-      numberOfReviews();
-    } else if (film.rating === 3) {
-      filmFullStar();
-      filmFullStar();
-      filmFullStar();
-      filmHollowStar();
-      filmHollowStar();
-      numberOfReviews();
-    } else if (film.rating === 4) {
-      filmFullStar();
-      filmFullStar();
-      filmFullStar();
-      filmFullStar();
-      filmHollowStar();
-      numberOfReviews();
-    } else if (film.rating === 5) {
-      filmFullStar();
-      filmFullStar();
-      filmFullStar();
-      filmFullStar();
-      filmFullStar();
-      numberOfReviews();
+      if (film.rating === 1) {
+        filmFullStar();
+        filmHollowStar();
+        filmHollowStar();
+        filmHollowStar();
+        filmHollowStar();
+        numberOfReviews();
+      } else if (film.rating === 2) {
+        filmFullStar();
+        filmFullStar();
+        filmHollowStar();
+        filmHollowStar();
+        filmHollowStar();
+        numberOfReviews();
+      } else if (film.rating === 3) {
+        filmFullStar();
+        filmFullStar();
+        filmFullStar();
+        filmHollowStar();
+        filmHollowStar();
+        numberOfReviews();
+      } else if (film.rating === 4) {
+        filmFullStar();
+        filmFullStar();
+        filmFullStar();
+        filmFullStar();
+        filmHollowStar();
+        numberOfReviews();
+      } else if (film.rating === 5) {
+        filmFullStar();
+        filmFullStar();
+        filmFullStar();
+        filmFullStar();
+        filmFullStar();
+        numberOfReviews();
+      }
+    }
+  } else if (document.getElementById("detail-article")) {
+    const whatArticle = getQueryParam("id");
+
+    const articleResponse = await fetch("data/articles.json");
+    const articleJSON = await articleResponse.json();
+    articles = articleJSON.articles;
+
+    const article = articles.find((a) => a.title == whatArticle);
+
+    if (article) {
+      articleImg.src = article.icon;
     }
   }
 });
