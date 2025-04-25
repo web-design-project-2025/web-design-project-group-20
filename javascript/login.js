93a4cef2368602c391020c584c7d92b193377e46
Ebba555 <ceeb24wi@student.ju.se>
2025-04-25 17:24:31 +0200

    log in now works!

--- javascript/login.js
+++ javascript/login.js
@@ -2,11 +2,27 @@ const loginButton = document.getElementById("login-button");
 const loginEmail = document.getElementById("email");
 const loginPassword = document.getElementById("password");
 
-loginButton.addEventListener ("click", function (event)) {
-const email = loginEmail.value;
-const password = loginPassword.value;
+loginButton.addEventListener("click", function (event) {
+  const email = loginEmail.value;
+  const password = loginPassword.value;
 
-if (email && pasword){
+  if (email && password) {
     let users = JSON.parse(localStorage.getItem("users")) || [];
-}
-}
+
+    /* the lines 15-16 and 19-20 are taken from chatgpt,
+here is the link : https://chatgpt.com/share/680ba771-1378-800a-89eb-d31095089275 */
+
+    const user = users.find(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (user) {
+      localStorage.setItem("loggedInUser", JSON.stringify(user));
+      alert("Login Successul!");
+    } else {
+      alert("Invalid email or password, please retry.");
+    }
+  } else {
+    alert("Please enter both mail and password to log in.");
+  }
+});
