329a21c13aaba1165cca0d44f74b0f53ef24bf25
wilma.nyren <nywi24wv@student.ju.se>
2025-04-28 17:01:12 +0200

    query selectors added

--- javascript/hero.js
+++ javascript/hero.js
@@ -1,1 +1,7 @@
-const imageElement = document.getElementById("ero-image");
+const imageElement = document.getElementById("hero-image");
+const movieTitleQuery = document.querySelector("timovie-titletle");
+const ratingQuery = document.querySelector("rating");
+const genreQuery = document.querySelector("genres");
+const descriptionQuery = document.querySelector("description");
+const linkToDetailQuery = document.querySelector("link-to-detail");
+
